e05edd30d67a23caad30165d133bd7037e9e716e
cheng.zuo <30d60cc46434bb05a29151795d3234d4c29ef4d6>
2021-12-07 14:06:00 +0800

    wip

--- vars/githubCheckout.groovy
+++ vars/githubCheckout.groovy
@@ -1,7 +1,7 @@
-def call(String project, String remoteHost = '10.1.2.192') {
+def call(String project, String defaultBranch = 'master', String remoteHost = '10.1.2.192') {
     timestamps {
         checkout([$class           : 'GitSCM',
-                  branches         : [[name: "${params.branch ?: sha1}"]],
+                  branches         : [[name: "${params.branch ?: sha1 ?: defaultBranch}"]],
                   userRemoteConfigs: [[credentialsId: "${params.certificate ?: 'kyligence-git'}",
                                        url          : "git://${remoteHost}/${params.repo ?: 'Kyligence'}/${project}.git"]],
                   browser          : [$class: 'GithubWeb', repoUrl: "https://github.com/Kyligence/${project}"],
