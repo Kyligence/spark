f44e7b141d87348c1215087e233f326cc0321a95
Cheng Zuo <Cheng.zuo@kyligence.io>
2021-12-10 20:01:11 +0800

    GitHub checkout (#7)
    * fix githubCheckout
    
    * fix githubCheckout
    
    Co-authored-by: cheng.zuo <30d60cc46434bb05a29151795d3234d4c29ef4d6>

--- vars/githubCheckout.groovy
+++ vars/githubCheckout.groovy
@@ -1,8 +1,10 @@
-def call(String project, String defaultBranch = 'master', String remoteHost = '10.1.2.192') {
+def call(String project, String remoteHost = '10.1.2.192', refspec = '+refs/heads/*:refs/remotes/origin/*', String defaultBranch = 'master') {
     timestamps {
         checkout([$class           : 'GitSCM',
                   branches         : [[name: "${params.branch ?: sha1 ?: defaultBranch}"]],
                   userRemoteConfigs: [[credentialsId: "${params.certificate ?: 'kyligence-git'}",
+                                       name         : "origin",
+                                       refspec      : "${refspec}",
                                        url          : "git://${remoteHost}/${params.repo ?: 'Kyligence'}/${project}.git"]],
                   browser          : [$class: 'GithubWeb', repoUrl: "https://github.com/Kyligence/${project}"],
                   extensions       : [
