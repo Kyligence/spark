a459d17d1949c13487868e761f5feb061ed16452
cheng.zuo <30d60cc46434bb05a29151795d3234d4c29ef4d6>
2021-12-13 17:00:46 +0800

    retest

---
+++ vars/retestFailedTests.groovy
@@ -0,0 +1,38 @@
+import hudson.tasks.junit.TestResultAction
+
+@NonCPS
+def call() {
+    def testResultActions = currentBuild.rawBuild.getActions(TestResultAction.class)
+    if (testResultActions.isEmpty()) {
+        echo 'Test result not found...'
+        return
+    }
+
+    def lastTestResultAction = testResultActions.last()
+    if (lastTestResultAction.getFailedTests().isEmpty()) {
+        echo 'No Failed Tests...'
+        return
+    }
+
+    echo "${lastTestResultAction.getFailCount()} failed tests will retesting..."
+    echo "${lastTestResultAction.getFailedTests().collect({ "${it.className}.${it.testName}" }).join('\n')}"
+
+    container('maven') {
+        def willRetestCases = lastTestResultAction.getFailedTests().collect({ "${it.className}#${it.testName}" }).join(',')
+        def testStage = 'RTest-Stage'
+        sh script: """
+            if [ ! -d ${testStage} ]; then
+                mkdir ${testStage} && cp -arf ./sourcecode/* ./${testStage}/
+            fi
+
+            cd ./${testStage} && mvn clean test --fail-at-end \
+            -Dtest='${willRetestCases}' \
+            -DfailIfNoTests=false \
+            -Duser.timezone=GMT+8
+            
+            # always return zero in retest
+            exit 0
+        """
+        junit skipPublishingChecks: true, testResults: "${testStage}/**/target/surefire-reports/*.xml"
+    }
+}
