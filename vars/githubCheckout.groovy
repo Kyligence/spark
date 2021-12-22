dc4edf2cb94340fed0c4ed898f9b131c3a9fd2c8
zuoc <cheng.zuo@kyligence>
2021-12-22 12:16:47 +0800

    wip

--- vars/githubCheckout.groovy
+++ vars/githubCheckout.groovy
@@ -1,11 +1,11 @@
-def call(String project, String remoteHost = '10.1.2.192', refspec = '+refs/heads/*:refs/remotes/origin/*', String defaultBranch = 'master') {
+def call(String project, String schema = 'git', String remoteHost = '10.1.2.192', refspec = '+refs/heads/*:refs/remotes/origin/*', String defaultBranch = 'master') {
     timestamps {
         checkout([$class           : 'GitSCM',
                   branches         : [[name: "${params.branch ?: sha1 ?: defaultBranch}"]],
                   userRemoteConfigs: [[credentialsId: "${params.certificate ?: 'kyligence-git'}",
                                        name         : "origin",
                                        refspec      : "${refspec}",
-                                       url          : "git://${remoteHost}/${params.repo ?: 'Kyligence'}/${project}.git"]],
+                                       url          : "${schema}://${remoteHost}/${params.repo ?: 'Kyligence'}/${project}.git"]],
                   browser          : [$class: 'GithubWeb', repoUrl: "https://github.com/Kyligence/${project}"],
                   extensions       : [
                           [$class: 'CleanBeforeCheckout'],
