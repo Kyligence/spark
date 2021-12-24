bbac639030438b24218b4c06cabd0ed227f9d24b
zuoc <cheng.zuo@kyligence>
2021-12-24 12:25:22 +0800

    Revert "minor"
    This reverts commit aace40bf6d2ff19358802a3a81b14e733caf076c.

--- vars/githubCheckout.groovy
+++ vars/githubCheckout.groovy
@@ -1,7 +1,7 @@
 def call(String project, String schema = 'git', String remoteHost = '10.1.2.192', refspec = '+refs/heads/*:refs/remotes/origin/*', String defaultBranch = 'master') {
     timestamps {
         checkout([$class           : 'GitSCM',
-                  branches         : [[name: "${sha1 ?: params.branch ?: defaultBranch}"]],
+                  branches         : [[name: "${params.branch ?: sha1 ?: defaultBranch}"]],
                   userRemoteConfigs: [[credentialsId: "${params.certificate ?: 'kyligence-git'}",
                                        name         : "origin",
                                        refspec      : "${refspec}",
