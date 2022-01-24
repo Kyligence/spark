4e9e5121a4d8cde4decf29321614df145e04c261
zuoc <cheng.zuo@kyligence>
2022-01-24 11:10:31 +0800

    fix newten-dev

--- vars/simpleNewtenParallelTest.groovy
+++ vars/simpleNewtenParallelTest.groovy
@@ -5,6 +5,7 @@ def call() {
             "src/core-job",
             "src/core-storage",
             "src/core-metadata",
+            "yinglong-enterprise-core-metadata",
             "src/query",
             "src/smart",
             "src/source-hive",
