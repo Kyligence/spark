3e777888a02aff4b4df2e92b2768712fdbed5039
sunfuzhou <sunfuzhou@outlook.com>
2022-02-09 15:42:25 +0800

    Update localGithubCheckOutCommitID.groovy

--- vars/localGithubCheckOutCommitID.groovy
+++ vars/localGithubCheckOutCommitID.groovy
@@ -1,7 +1,7 @@
 def call(String project, String branch = null, String schema = 'git', String remoteHost = '10.1.2.192', refspec = '+refs/heads/*:refs/remotes/origin/*') {
     timestamps {
         checkout([$class           : 'GitSCM',
-                  branches         : [[name: "${branch ?: params.branch ?: sha1}"]],
+                  branches         : [[name: "${commitid ?: params.commitid ?: sha1}"]],
                   userRemoteConfigs: [[credentialsId: "${params.certificate ?: 'kyligence-git'}",
                                        name         : "origin",
                                        refspec      : "${refspec}",
