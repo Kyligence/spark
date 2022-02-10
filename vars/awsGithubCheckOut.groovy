33b9a463517e33e2cc9ffc5019146de4ea6073e5
sunfuzhou <sunfuzhou@outlook.com>
2022-02-10 15:02:04 +0800

    Update awsGithubCheckOut.groovy

--- vars/awsGithubCheckOut.groovy
+++ vars/awsGithubCheckOut.groovy
@@ -2,7 +2,7 @@ def call(String project, String branch = null, String schema = 'git', String rem
     timestamps {
         checkout([$class           : 'GitSCM',
                   branches         : [[name: "${branch ?: params.branch ?: sha1}"]],
-                  userRemoteConfigs: [[credentialsId: "${params.certificate ?: 'kyligence-git'}",
+                  userRemoteConfigs: [[credentialsId: "${params.certificate ?: 'kyligencegit'}",
                                        name         : "origin",
                                        refspec      : "${refspec}",
                                        url          : "${schema}://${remoteHost}/${params.repo ?: 'Kyligence'}/${project}.git"]],
