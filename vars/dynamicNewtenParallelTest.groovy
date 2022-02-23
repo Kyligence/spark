faf180463f7ec7cf047ecc30fda8a11fb4210981
Enwei Jiao <enwei.jiao@kyligence.io>
2022-02-23 15:39:10 +0800

    fix coverage

--- vars/dynamicNewtenParallelTest.groovy
+++ vars/dynamicNewtenParallelTest.groovy
@@ -1,33 +1,35 @@
 import java.util.concurrent.LinkedBlockingQueue
 
 def call() {
-    def modules = sh(script:'''
-            cd sourcecode
-            mvn help:evaluate -Dexpression=project.modules | grep -v "^\\[" | grep -v "<\\/*strings>" | sed 's/<\\/*string>//g' | sed 's/[[:space:]]//'
-        ''', returnStdout: true).trim().split("\n").collect({ it.trim() }).findAll { it.startsWith("src") }
-    def blacklistModules = ['src/kap-it', 'src/server-base', 'src/second-storage/clickhouse-it']
-    def taskQueue = [
-        "src/kap-it -Dtest='!io.kyligence.kap.newten.auto.NAutoBuildAndQueryTest'",
-        "src/kap-it -Dtest='io.kyligence.kap.newten.auto.NAutoBuildAndQueryTest'",
-        "src/server-base",
-        "src/second-storage/clickhouse-it"] as LinkedBlockingQueue
-    modules.each { m ->
-        if (!blacklistModules.contains(m)) {
-            taskQueue.add(m)
+    container('maven') {
+        def modules = sh(script:'''
+                cd sourcecode
+                mvn help:evaluate -Dexpression=project.modules | grep -v "^\\[" | grep -v "<\\/*strings>" | sed 's/<\\/*string>//g' | sed 's/[[:space:]]//'
+            ''', returnStdout: true).trim().split("\n").collect({ it.trim() }).findAll { it.startsWith("src") }
+        def blacklistModules = ['src/kap-it', 'src/server-base', 'src/second-storage/clickhouse-it']
+        def taskQueue = [
+            "src/kap-it -Dtest='!io.kyligence.kap.newten.auto.NAutoBuildAndQueryTest'",
+            "src/kap-it -Dtest='io.kyligence.kap.newten.auto.NAutoBuildAndQueryTest'",
+            "src/server-base",
+            "src/second-storage/clickhouse-it"] as LinkedBlockingQueue
+        modules.each { m ->
+            if (!blacklistModules.contains(m)) {
+                taskQueue.add(m)
+            }
         }
+
+        taskQueue.each {
+            echo "found modele " + it
+        }
+
+        def worker1 = createWorker("UTest-Stage-1", taskQueue)
+        def worker2 = createWorker("UTest-Stage-2", taskQueue)
+        def worker3 = createWorker("UTest-Stage-3", taskQueue)
+        def worker4 = createWorker("UTest-Stage-4", taskQueue)
+
+        def allTestStages = worker1 + worker2 + worker3 + worker4
+        parallel allTestStages
     }
-
-    taskQueue.each {
-        echo "found modele " + it
-    }
-
-    def worker1 = createWorker("UTest-Stage-1", taskQueue)
-    def worker2 = createWorker("UTest-Stage-2", taskQueue)
-    def worker3 = createWorker("UTest-Stage-3", taskQueue)
-    def worker4 = createWorker("UTest-Stage-4", taskQueue)
-
-    def allTestStages = worker1 + worker2 + worker3 + worker4
-    parallel allTestStages
 }
 
 def createWorker(String stage, LinkedBlockingQueue taskQueue) {
@@ -48,9 +50,6 @@ def createWorker(String stage, LinkedBlockingQueue taskQueue) {
                         -DfailIfNoTests=false \
                         -Duser.timezone=GMT+8  ${params.args}
                     """
-                    // sh script: """
-                    //     cp -rf ./${stage}/${item}/target ./sourcecode/${item}/
-                    // """
                     item = taskQueue.poll()
                 }
             }
--- vars/s3Archive.groovy
+++ vars/s3Archive.groovy
@@ -8,43 +8,57 @@ def call(String type) {
 
 def uploadArchive() {
     timestamps {
-        withAWS(region: 'us-west-2', credentials: 'aws_global_s3_cp') {
-            // Upload reports for analyze
-            sh script: """
-                mkdir -p ${BUILD_NUMBER}/report
-                mkdir -p ${BUILD_NUMBER}/site
-                outputs=\$(find "\$(pwd)" -path '*surefire-reports/*.xml' | sed 's/.*/&/')
-                for out in \$outputs; do
-                    cp \$out ${BUILD_NUMBER}/report/
-                done
-                outputs=\$(find "\$(pwd)" -path '*target*site' | sed 's/.*/&/')
-                for out in \$outputs; do
-                    target_out=\$(echo \$out | rev |cut -d/ -f 3 | rev)
-                    cp -r \$out ${BUILD_NUMBER}/site/\$target_out
-                done
-                tar czf ${BUILD_NUMBER}.tar.gz ${BUILD_NUMBER}/
-            """
-            s3Upload(bucket: 'k8s-bucket-devops', path: "ke-ci-result/${JOB_NAME}/", includePathPattern: "${BUILD_NUMBER}.tar.gz")
-        }
-        if (defaultJvmArgs().contains('-DpersistBuild=true')) {
-            // Upload repository cache for speedup maven install 
-            sh script: """
-                cp -r /jenkins-common/.kem2/repository .
-                tar czf repository.tar.gz repository
-            """
-            s3Upload(file: 'repository.tar.gz', bucket: 'k8s-bucket-devops', path: "ke-ci-repository/${targetBranch()}/repository.tar.gz", force: true)
-
-            // Upload Build Result
-            sh script: """
-                mkdir -p ${targetBranch()}
-                outputs=\$(find "\$(pwd)" -path '*test_data/*.zip' | sed 's/.*/&/')
-                for out in \$outputs; do
-                    cp \$out ${targetBranch()}
-                done
-            """
+        try {
             withAWS(region: 'us-west-2', credentials: 'aws_global_s3_cp') {
-                s3Upload(bucket: 'k8s-bucket-devops', path: "ke-ci/", includePathPattern: "${targetBranch()}/*")
+                // Upload reports for analyze
+                sh script: """
+                    mkdir -p ${BUILD_NUMBER}/report
+                    mkdir -p ${BUILD_NUMBER}/site
+                    mkdir -p ${BUILD_NUMBER}/exec
+                    outputs=\$(find "\$(pwd)" -path '*surefire-reports/*.xml' | sed 's/.*/&/')
+                    for out in \$outputs; do
+                        cp \$out ${BUILD_NUMBER}/report/
+                    done
+
+                    i=1
+                    outputs=\$(find "\$(pwd)" -path '*Test-Stage-*site' | sed 's/.*/&/')
+                    for out in \$outputs; do
+                        target_out=\$(echo \$out | rev |cut -d/ -f 3 | rev)
+                        cp -r \$out ${BUILD_NUMBER}/site/"\$target_out-\$i"
+                        i=\$((i+1))
+                    done
+                    outputs=\$(find "\$(pwd)" -path '*Test-Stage-*jacoco.exec' | sed 's/.*/&/')
+                    for out in \$outputs; do
+                        target_out=\$(echo \$out | rev |cut -d/ -f 3 | rev)
+                        cp -r \$out ${BUILD_NUMBER}/exec/"\$target_out-\$i-jacoco.exec"
+                        i=\$((i+1))
+                    done
+                    tar czf ${BUILD_NUMBER}.tar.gz ${BUILD_NUMBER}/
+                """
+                s3Upload(bucket: 'k8s-bucket-devops', path: "ke-ci-result/${JOB_NAME}/", includePathPattern: "${BUILD_NUMBER}.tar.gz")
             }
+            if (defaultJvmArgs().contains('-DpersistBuild=true')) {
+                // Upload repository cache for speedup maven install 
+                sh script: """
+                    cp -r /jenkins-common/.kem2/repository .
+                    tar czf repository.tar.gz repository
+                """
+                s3Upload(file: 'repository.tar.gz', bucket: 'k8s-bucket-devops', path: "ke-ci-repository/${targetBranch()}/repository.tar.gz", force: true)
+
+                // Upload Build Result
+                sh script: """
+                    mkdir -p ${targetBranch()}
+                    outputs=\$(find "\$(pwd)" -path '*test_data/*.zip' | sed 's/.*/&/')
+                    for out in \$outputs; do
+                        cp \$out ${targetBranch()}
+                    done
+                """
+                withAWS(region: 'us-west-2', credentials: 'aws_global_s3_cp') {
+                    s3Upload(bucket: 'k8s-bucket-devops', path: "ke-ci/", includePathPattern: "${targetBranch()}/*")
+                }
+            }
+        } catch (Exception err) {
+            echo err.getMessage()
         }
     }
 }
@@ -68,6 +82,7 @@ def downloadArchive() {
                 fi
                 rm -rf ./tmp
             """
+            s3Download(file: 'coverage/jacococli.jar', bucket: 'k8s-bucket-devops', path: "ke-ci/jacococli.jar", force: true)
         }
     }
 }
