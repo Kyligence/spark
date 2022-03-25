8db7ee3cd846bebc526e90199f48852c54e2abe4
sunfuzhou <sunfuzhou@outlook.com>
2022-03-25 11:01:03 +0800

    master_update_var_yam_by_sfz (#120)
    Co-authored-by: sunfuzhou <fuzhou.sun@kyligence.com>

--- vars/buildNewten.groovy
+++ vars/buildNewten.groovy
@@ -1,21 +1,21 @@
 def call(String version, boolean skipPatch = true, boolean skipLicense = true) {
     sh "git apply license.patch"
     sh """
-        wget http://mvn.ops.kylincorp.com/repository/raw-tars-hosted/io.kyligence.ke/grafana-6.2.4.linux-amd64.tar.gz --directory-prefix=build/
-        wget http://mvn.ops.kylincorp.com/repository/raw-tars-hosted/io.kyligence.ke/influxdb-1.6.4.x86_64.rpm --directory-prefix=build/
-        wget http://mvn.ops.kylincorp.com/repository/raw-tars-hosted/io.kyligence.spark/spark-newten-3.2.0-4.x-r56.tgz --directory-prefix=build/
+        wget http://10.1.9.200:8081/repository/raw-tars-hosted/io.kyligence.ke/grafana-6.2.4.linux-amd64.tar.gz --directory-prefix=build/
+        wget http://10.1.9.200:8081/repository/raw-tars-hosted/io.kyligence.ke/influxdb-1.6.4.x86_64.rpm --directory-prefix=build/
+        wget http://10.1.9.200:8081/repository/raw-tars-hosted/io.kyligence.spark/spark-newten-3.2.0-4.x-r56.tgz --directory-prefix=build/
 
-        wget http://mvn.ops.kylincorp.com/repository/raw-tars-hosted/io.kyligence.ke/postgresql10-10.7-1PGDG.rhel6.x86_64.rpm --directory-prefix=build/postgresql/
-        wget http://mvn.ops.kylincorp.com/repository/raw-tars-hosted/io.kyligence.ke/postgresql10-libs-10.7-1PGDG.rhel6.x86_64.rpm --directory-prefix=build/postgresql/
-        wget http://mvn.ops.kylincorp.com/repository/raw-tars-hosted/io.kyligence.ke/postgresql10-server-10.7-1PGDG.rhel6.x86_64.rpm --directory-prefix=build/postgresql/
+        wget http://10.1.9.200:8081/repository/raw-tars-hosted/io.kyligence.ke/postgresql10-10.7-1PGDG.rhel6.x86_64.rpm --directory-prefix=build/postgresql/
+        wget http://10.1.9.200:8081/repository/raw-tars-hosted/io.kyligence.ke/postgresql10-libs-10.7-1PGDG.rhel6.x86_64.rpm --directory-prefix=build/postgresql/
+        wget http://10.1.9.200:8081/repository/raw-tars-hosted/io.kyligence.ke/postgresql10-server-10.7-1PGDG.rhel6.x86_64.rpm --directory-prefix=build/postgresql/
 
-        wget http://mvn.ops.kylincorp.com/repository/raw-tars-hosted/io.kyligence.ke/postgresql10-10.7-1PGDG.rhel7.x86_64.rpm --directory-prefix=build/postgresql/
-        wget http://mvn.ops.kylincorp.com/repository/raw-tars-hosted/io.kyligence.ke/postgresql10-libs-10.7-1PGDG.rhel7.x86_64.rpm --directory-prefix=build/postgresql/
-        wget http://mvn.ops.kylincorp.com/repository/raw-tars-hosted/io.kyligence.ke/postgresql10-server-10.7-1PGDG.rhel7.x86_64.rpm --directory-prefix=build/postgresql/
+        wget http://10.1.9.200:8081/repository/raw-tars-hosted/io.kyligence.ke/postgresql10-10.7-1PGDG.rhel7.x86_64.rpm --directory-prefix=build/postgresql/
+        wget http://10.1.9.200:8081/repository/raw-tars-hosted/io.kyligence.ke/postgresql10-libs-10.7-1PGDG.rhel7.x86_64.rpm --directory-prefix=build/postgresql/
+        wget http://10.1.9.200:8081/repository/raw-tars-hosted/io.kyligence.ke/postgresql10-server-10.7-1PGDG.rhel7.x86_64.rpm --directory-prefix=build/postgresql/
 
-        wget http://mvn.ops.kylincorp.com/repository/raw-tars-hosted/io.kyligence.ke/postgresql10-10.8-1PGDG.rhel8.x86_64.rpm --directory-prefix=build/postgresql/
-        wget http://mvn.ops.kylincorp.com/repository/raw-tars-hosted/io.kyligence.ke/postgresql10-libs-10.8-1PGDG.rhel8.x86_64.rpm --directory-prefix=build/postgresql/
-        wget http://mvn.ops.kylincorp.com/repository/raw-tars-hosted/io.kyligence.ke/postgresql10-server-10.8-1PGDG.rhel8.x86_64.rpm --directory-prefix=build/postgresql/
+        wget http://10.1.9.200:8081/repository/raw-tars-hosted/io.kyligence.ke/postgresql10-10.8-1PGDG.rhel8.x86_64.rpm --directory-prefix=build/postgresql/
+        wget http://10.1.9.200:8081/repository/raw-tars-hosted/io.kyligence.ke/postgresql10-libs-10.8-1PGDG.rhel8.x86_64.rpm --directory-prefix=build/postgresql/
+        wget http://10.1.9.200:8081/repository/raw-tars-hosted/io.kyligence.ke/postgresql10-server-10.8-1PGDG.rhel8.x86_64.rpm --directory-prefix=build/postgresql/
     """
     sh "export release_version=${version} && sh build/script_newten/release.sh -noTimestamp"
 }
--- vars/pushRawArtifactsByApi.groovy
+++ vars/pushRawArtifactsByApi.groovy
@@ -1,6 +1,6 @@
 def call(repository,directory,file,filename,user,passwd){
  sh """
-   curl -X POST "http://10.1.9.11:8081/service/rest/v1/components?repository=${repository}" \
+   curl -X POST "http://10.1.9.200:8081/service/rest/v1/components?repository=${repository}" \
       -H "accept: application/json" \
       -H "Content-Type: multipart/form-data" \
       -F "raw.directory=${directory}" \
