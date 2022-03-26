f3706a382abdb2be7d1e015f6351b2ebf4851a32
lex <lex@dawnm.tech>
2022-03-26 15:48:47 +0800

    add retry for sonar

--- vars/sonarAnalyze.groovy
+++ vars/sonarAnalyze.groovy
@@ -1,6 +1,6 @@
 def call(String sonarArgLine) {
-    container("sonar-cloud") {
-        dir("sourcecode") {
+    container('sonar-cloud') {
+        dir('sourcecode') {
             withSonarQubeEnv(credentialsId: 'sonar_credential', installationName: 'sonar cloud') {
                 // You can override the credential to be used
                 sh script: """
@@ -15,24 +15,28 @@ def call(String sonarArgLine) {
                     done
                 """
 
-                dir("coverage") {
+                dir('coverage') {
                     script {
-                        try { sh script: "cp ../src/server/target/jars/kap-* jars/" } catch (Exception err) { }
-                        try { sh script: "cp ../src/server/target/jars/yinglong-* jars/" } catch (Exception err) { }
+                        try { sh script: 'cp ../src/server/target/jars/kap-* jars/' } catch (Exception err) { }
+                        try { sh script: 'cp ../src/server/target/jars/yinglong-* jars/' } catch (Exception err) { }
                         // try { sh script: "cp ../src/server/target/jars/ke-* jars/" } catch (Exception err) { }
-                        try { sh script: "rm -f jars/kap-external-curator-* jars/kap-external-guava20-* jars/kap-external-swagger-* jars/kap-external-influxdb-*" } catch (Exception err) { }
-                        try { sh script: "rm -f jars/*-tests.jar" } catch (Exception err) { }
-                        sh script: "java -jar jacococli.jar report ./exec/* --html output --classfiles jars/ --xml jacoco.xml"
+                        try { sh script: 'rm -f jars/kap-external-curator-* jars/kap-external-guava20-* jars/kap-external-swagger-* jars/kap-external-influxdb-*' } catch (Exception err) { }
+                        try { sh script: 'rm -f jars/*-tests.jar' } catch (Exception err) { }
+                        retry(3){
+                            sh script: 'java -jar jacococli.jar report ./exec/* --html output --classfiles jars/ --xml jacoco.xml'
+                        }
                     }
                 }
-                sh script: """
-                    mvn sonar:sonar -T 4C \
-                        -Dsonar.host.url=https://sonarcloud.io \
-                        -Dsonar.organization=kyligence \
-                        -Dsonar.projectKey=Kyligence_KAP \
-                        ${sonarArgLine} \
-                        -Dsonar.coverage.jacoco.xmlReportPaths=${WORKSPACE}/sourcecode/coverage/jacoco.xml
-                """
+                retry(3) {
+                    sh script: """
+                        mvn sonar:sonar -T 4C \
+                            -Dsonar.host.url=https://sonarcloud.io \
+                            -Dsonar.organization=kyligence \
+                            -Dsonar.projectKey=Kyligence_KAP \
+                            ${sonarArgLine} \
+                            -Dsonar.coverage.jacoco.xmlReportPaths=${WORKSPACE}/sourcecode/coverage/jacoco.xml
+                    """
+                }
             }
         }
     }
