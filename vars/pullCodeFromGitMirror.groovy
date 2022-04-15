55fd49ba9abe03fbbe6ff92868006dfdffd773d5
lex <lex@dawnm.tech>
2022-04-15 22:54:00 +0800

    handbook finish

---
+++ vars/pullCodeFromGitMirror.groovy
@@ -0,0 +1,25 @@
+def call(String project, String branch, Boolean fromRemote=false) {
+    println "param: branch/commit: ${branch}"
+    if (!branch) {
+        error 'branch 参数不能为空'
+    }
+
+    if(!fromRemote) { // 从本地镜像获取
+        println 'Sync the git mirror'
+        sshCmdWithCredentials(
+            'bash -c /var/lib/git/pull.sh',
+            'private_git',
+            ['name':'localhost', 'host':'10.1.2.192', 'allowAnyHosts': true]
+        )
+
+        println "checkout ${project} from git mirror at local, it'll be retry 3 times"
+        retry(3) {
+            githubCheckout project, branch
+        }
+    } else {
+        println "checkout ${project} from github from remote, it'll be retry 3 times"
+        retry(3) {
+            githubCheckout project, branch, 'https', 'github.com'
+        }
+    }
+}
---
+++ vars/sendNotify.groovy
@@ -0,0 +1,33 @@
+def call(String way,String subject,String to,String body,String from=null) {
+
+    switch(way) {
+        case 'EMAIL':
+            sendEmail(subject,to,body,from)
+        break
+        case 'FEISHU':
+            sendFeishu(subject,to,body,from)
+        break
+        case 'SMS':
+            sendSms(subject,to,body,from)
+        break
+    }
+}
+
+// way == EMAIL
+def sendEmail(subject,to,body,from=null) {
+    emailext (
+        subject: subject,
+        to: to,
+        from: from ?: 'devops_support@kyligence.io',
+        //定义html 模版
+        body: body
+    )
+}
+
+def sendFeishu(subject,to,body,from) {
+    println "飞书提醒还未实现，情景期待"
+}
+
+def sendSms(subject,to,body,from) {
+    println "短信提醒还未实现，情景期待"
+}
