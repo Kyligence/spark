d746925d6c910501121e8b7b6943f1b56b44c97b
lex <lex@dawnm.tech>
2022-04-17 22:32:05 +0800

    fix git pull bug

--- vars/pullCodeFromGitMirror.groovy
+++ vars/pullCodeFromGitMirror.groovy
@@ -7,7 +7,7 @@ def call(String project, String branch, Boolean fromRemote=false) {
     if(!fromRemote) { // 从本地镜像获取
         println 'Sync the git mirror'
         sshCmdWithCredentials(
-            'bash -c /var/lib/git/pull.sh',
+            'sh /var/lib/git/pull.sh',
             'private_git',
             ['name':'localhost', 'host':'10.1.2.192', 'allowAnyHosts': true]
         )
