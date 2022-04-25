2274dab635678cb0e61d07a179fd91a62866351d
sunbiaobiao <sunbiaobiaooo@gmail.com>
2022-04-25 15:21:14 +0800

    remote  download spark package in pipeline (#133)

--- vars/buildNewten.groovy
+++ vars/buildNewten.groovy
@@ -3,7 +3,6 @@ def call(String version, boolean noSpark = false) {
     sh """
         wget http://10.1.9.200:8081/repository/raw-tars-hosted/io.kyligence.ke/grafana-6.2.4.linux-amd64.tar.gz --directory-prefix=build/
         wget http://10.1.9.200:8081/repository/raw-tars-hosted/io.kyligence.ke/influxdb-1.6.4.x86_64.rpm --directory-prefix=build/
-        wget http://10.1.9.200:8081/repository/raw-tars-hosted/io.kyligence.spark/spark-newten-3.2.0-4.x-r56.tgz --directory-prefix=build/
 
         wget http://10.1.9.200:8081/repository/raw-tars-hosted/io.kyligence.ke/postgresql10-10.7-1PGDG.rhel6.x86_64.rpm --directory-prefix=build/postgresql/
         wget http://10.1.9.200:8081/repository/raw-tars-hosted/io.kyligence.ke/postgresql10-libs-10.7-1PGDG.rhel6.x86_64.rpm --directory-prefix=build/postgresql/
