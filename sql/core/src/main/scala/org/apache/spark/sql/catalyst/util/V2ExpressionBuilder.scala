50e71f97415ca3c3e0e86f2d9486bfd00ab99d17
chenzhx <chen@apache.org>
2022-06-10 13:41:23 +0800

    Revert "KE-37052 translate boolean column to V2Predicate (#477)"
    This reverts commit 7796f199817562ee4bb541f391617dfab3bb52d3.

--- sql/core/src/main/scala/org/apache/spark/sql/catalyst/util/V2ExpressionBuilder.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/catalyst/util/V2ExpressionBuilder.scala
@@ -56,12 +56,8 @@ class V2ExpressionBuilder(
       } else {
         Some(FieldReference(name))
       }
-    case col @ pushableColumn(name) if !nestedPredicatePushdownEnabled =>
-      if (isPredicate && col.dataType.isInstanceOf[BooleanType]) {
-        Some(new V2Predicate("=", Array(FieldReference(name), LiteralValue(true, BooleanType))))
-      } else {
-        Some(FieldReference(name))
-      }
+    case pushableColumn(name) if !nestedPredicatePushdownEnabled =>
+      Some(FieldReference(name))
     case in @ InSet(child, hset) =>
       generateExpression(child).map { v =>
         val children =
