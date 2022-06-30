a00f9c364b5870a00d123e09155f5da04c577ddd
Lex Meng <liming.meng@kyligence.io>
2022-06-30 13:31:08 +0800

    set download cache

--- vars/buildNewten.groovy
+++ vars/buildNewten.groovy
@@ -2,6 +2,8 @@ def call(String version, boolean noSpark = false, String docs_commitid = 'latest
     sh "git apply license.patch"
     sh """
         wget http://repo-ofs.kyligence.tech:8081/repository/raw-tars-hosted/io.kyligence.ke/grafana-6.2.4.linux-amd64.tar.gz --directory-prefix=build/
+        wget http://repo-ofs.kyligence.tech:8081/repository/raw-tars-hosted/io.kyligence.ke/hive_1_2_2.tar.gz --directory-prefix=build/
+
         wget http://repo-ofs.kyligence.tech:8081/repository/raw-tars-hosted/io.kyligence.ke/influxdb-1.6.4.x86_64.rpm --directory-prefix=build/
 
         wget http://repo-ofs.kyligence.tech:8081/repository/raw-tars-hosted/io.kyligence.ke/postgresql10-10.7-1PGDG.rhel6.x86_64.rpm --directory-prefix=build/postgresql/
@@ -15,6 +17,11 @@ def call(String version, boolean noSpark = false, String docs_commitid = 'latest
         wget http://repo-ofs.kyligence.tech:8081/repository/raw-tars-hosted/io.kyligence.ke/postgresql10-10.8-1PGDG.rhel8.x86_64.rpm --directory-prefix=build/postgresql/
         wget http://repo-ofs.kyligence.tech:8081/repository/raw-tars-hosted/io.kyligence.ke/postgresql10-libs-10.8-1PGDG.rhel8.x86_64.rpm --directory-prefix=build/postgresql/
         wget http://repo-ofs.kyligence.tech:8081/repository/raw-tars-hosted/io.kyligence.ke/postgresql10-server-10.8-1PGDG.rhel8.x86_64.rpm --directory-prefix=build/postgresql/
+
+        spark_version_pom=`mvn help:evaluate -Dexpression=spark.version | grep -E '^[0-9]+\.[0-9]+\.[0-9]+' `
+        spark_pkg_name=spark-newten-"`echo \$spark_version_pom | sed "s/-kylin//g"`"
+        
+        wget http://repo-ofs.kyligence.tech:8081/repository/raw-tars-hosted/io.kyligence.spark/\$spark_pkg_name.tgz --directory-prefix=build/
     """
     sh "npm cache verify"
 
