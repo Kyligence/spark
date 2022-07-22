a3008517a7ec0254ea27b448ca2de1d46aa4da39
Lex Meng <liming.meng@kyligence.io>
2022-07-22 19:20:22 +0800

    debug

--- vars/buildNewten.groovy
+++ vars/buildNewten.groovy
@@ -4,7 +4,8 @@ def call(String version, boolean noSpark = false, String docs_commitid = 'latest
         pwd
         ls -lh
         cat license.patch
-
+    """
+    sh """
         wget https://repo-ofs.kyligence.com/repository/raw-tars-hosted/io.kyligence.ke/grafana-6.2.4.linux-amd64.tar.gz --directory-prefix=build/
         wget https://repo-ofs.kyligence.com/repository/raw-tars-hosted/io.kyligence.ke/hive_1_2_2.tar.gz --directory-prefix=build/
 
