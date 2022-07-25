1fc50dd1e926440f518c1445953be21812262147
Lex Meng <liming.meng@kyligence.io>
2022-07-25 19:47:11 +0800

    add manual params

--- vars/buildNewten.groovy
+++ vars/buildNewten.groovy
@@ -1,10 +1,6 @@
 def call(String version, boolean noSpark = false, String docs_commitid = 'latest', String customerPkg = 'NORMAL') {
     sh "git apply license.patch"
-    sh """
-        pwd
-        ls -lh
-        cat license.patch
-    """
+    sh "pwd && ls -lha"
     sh """
         wget https://repo-ofs.kyligence.com/repository/raw-tars-hosted/io.kyligence.ke/grafana-6.2.4.linux-amd64.tar.gz --directory-prefix=build/
         wget https://repo-ofs.kyligence.com/repository/raw-tars-hosted/io.kyligence.ke/hive_1_2_2.tar.gz --directory-prefix=build/
