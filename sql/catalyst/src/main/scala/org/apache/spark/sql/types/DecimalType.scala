d4e13180f0880b1dbd1d686a5506f417b4000467
songzhxlh-max <59271430+songzhxlh-max@users.noreply.github.com>
2022-09-22 10:05:02 +0800

    AL-6084 in Cast for method of canCast, when DecimalType cast to DoubleType add transformable logic (#542)
    * AL-6084 in Cast for method of canCast, when DecimalType cast DecimalType to DoubleType add suit logical

--- sql/catalyst/src/main/scala/org/apache/spark/sql/types/DecimalType.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/types/DecimalType.scala
@@ -92,7 +92,10 @@ case class DecimalType(precision: Int, scale: Int) extends FractionalType {
    * Returns whether this DecimalType is tighter than `other`. If yes, it means `this`
    * can be casted into `other` safely without losing any precision or range.
    */
-  private[sql] def isTighterThan(other: DataType): Boolean = other match {
+  private[sql] def isTighterThan(other: DataType): Boolean = isTighterThanInternal(other)
+
+  @tailrec
+  private def isTighterThanInternal(other: DataType): Boolean = other match {
     case dt: DecimalType =>
       (precision - scale) <= (dt.precision - dt.scale) && scale <= dt.scale
     case dt: IntegralType =>
@@ -101,6 +104,8 @@ case class DecimalType(precision: Int, scale: Int) extends FractionalType {
       // If the precision equals `integerAsDecimal.precision`, there can be integer overflow
       // during casting.
       precision < integerAsDecimal.precision && scale == 0
+    case dt: DoubleType =>
+      isTighterThanInternal(DecimalType(precision, scale))
     case _ => false
   }
 
