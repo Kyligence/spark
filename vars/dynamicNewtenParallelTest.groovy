74a2e7fd5570cfb35912bdbc9eef19aa09e28175
Lex Meng <liming.meng@kyligence.io>
2022-09-30 09:47:29 +0800

    refine ut split

--- vars/dynamicNewtenParallelTest.groovy
+++ vars/dynamicNewtenParallelTest.groovy
@@ -13,10 +13,12 @@ def call() {
         if (kap_it_module) {
             modules.removeAll([kap_it_module])
             modules.addAll([
-                "${kap_it_module} -Dtest='!io.kyligence.kap.newten.auto.NAutoBuildAndQueryTest'",
+                // "${kap_it_module} -Dtest='!io.kyligence.kap.newten.auto.NAutoBuildAndQueryTest'",
                 "${kap_it_module} -Dtest='io.kyligence.kap.newten.auto.NAutoBuildAndQueryTest'"
             ])
         }
+
+        def no_query_module = ["${kap_it_module} -Dtest='!io.kyligence.kap.newten.auto.NAutoBuildAndQueryTest'"]
 
         def clickhouse_it_module = modules.findAll({ it.contains('src/second-storage/clickhouse-it')}).getAt(0)
         if(clickhouse_it_module) {
@@ -42,13 +44,15 @@ def call() {
         println "final modules: ${modules}"
 
         def taskQueue = modules as LinkedBlockingQueue
+        def specQueue = no_query_module as LinkedBlockingQueue
 
         def worker1 = createWorker("UTest-Stage-1", taskQueue)
         def worker2 = createWorker("UTest-Stage-2", taskQueue)
         def worker3 = createWorker("UTest-Stage-3", taskQueue)
         def worker4 = createWorker("UTest-Stage-4", taskQueue)
+        def worker5 = createWorker("UTest-Stage-5", specQueue)
 
-        def allTestStages = worker1 + worker2 + worker3 + worker4
+        def allTestStages = worker1 + worker2 + worker3 + worker4 + worker5
         allTestStages.failFast = true
         parallel allTestStages
     }
