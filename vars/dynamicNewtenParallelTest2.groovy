7d2db8765ebef0912099296635a3ffdf14015d58
Lex Meng <liming.meng@kyligence.io>
2022-10-25 16:51:07 +0800

    fix pom ut

--- vars/dynamicNewtenParallelTest2.groovy
+++ vars/dynamicNewtenParallelTest2.groovy
@@ -80,6 +80,7 @@ def createWorker(String stage, LinkedBlockingQueue taskQueue) {
                         sh script: """
                             cd ./${stage}
                             mvn clean test --fail-at-end \
+                            -Pclickhouse.native.jdbc,3rd,aze,sandbox
                             -pl ${item} \
                             -DfailIfNoTests=false \
                             -Duser.timezone=GMT+8 ${jvmArgs}
