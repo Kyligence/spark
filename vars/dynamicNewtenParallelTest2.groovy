579625fd692c5f3192d651f364cb03b6bec45713
Lex Meng <liming.meng@kyligence.io>
2022-10-25 19:03:25 +0800

    fix dockerfile

--- vars/dynamicNewtenParallelTest2.groovy
+++ vars/dynamicNewtenParallelTest2.groovy
@@ -80,7 +80,7 @@ def createWorker(String stage, LinkedBlockingQueue taskQueue) {
                         sh script: """
                             cd ./${stage}
                             mvn clean test --fail-at-end \
-                            -Paws,3rd,sandbox,clickhouse.native.jdbc
+                            -P aws,3rd,sandbox,clickhouse.native.jdbc
                             -pl ${item} \
                             -DfailIfNoTests=false \
                             -Duser.timezone=GMT+8 ${jvmArgs}
