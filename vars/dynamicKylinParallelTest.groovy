876f7956ac8b8c552090d8e1172b501d2680286c
Tengting Xu <34978943+Mukvin@users.noreply.github.com>
2022-10-27 19:33:51 +0800

    Add kylin5 ci (#256)
    * Add kylin5 ci

---
+++ vars/dynamicKylinParallelTest.groovy
@@ -0,0 +1,97 @@
+import java.util.concurrent.LinkedBlockingQueue
+import java.util.Collections
+
+def call() {
+    container('maven') {
+        def modules = sh(script: '''
+                cd sourcecode
+                mvn help:evaluate -Dexpression=project.modules | grep -v "^\\[" | grep -v "<\\/*strings>" | sed 's/<\\/*string>//g' | sed 's/[[:space:]]//'
+            ''', returnStdout: true).trim().split("\n").collect({ it.trim() }).findAll { it.startsWith("src") || it.startsWith("kylin/src") || it == "kylin" }
+        println "origin modules: ${modules}"
+
+//        def clickhouse_it_module = modules.findAll({ it.contains('src/second-storage/clickhouse-it')}).getAt(0)
+//        if(clickhouse_it_module) {
+//            modules.removeAll([clickhouse_it_module])
+//            clickhouse_it_module = [clickhouse_it_module]
+//        }
+
+        if ('kylin' in modules) {
+            def kylin_modules = sh(script: '''
+                cd sourcecode/kylin
+                mvn help:evaluate -Dexpression=project.modules | grep -v "^\\[" | grep -v "<\\/*strings>" | sed 's/<\\/*string>//g' | sed 's/[[:space:]]//'
+            ''', returnStdout: true).trim().split("\n").collect({ 'kylin/' + it.trim() }).findAll { it.startsWith("kylin/src") }
+            println "kylin sub modules: ${kylin_modules}"
+
+            modules.removeAll(['kylin'])
+            modules.addAll(kylin_modules)
+
+//            def _clickhouse_it_module = kylin_modules.findAll({ it.contains('src/second-storage/clickhouse-it')}).getAt(0)
+//            println "_clickhouse_it_module: ${_clickhouse_it_module}"
+//
+//            if(_clickhouse_it_module) {
+//                modules.removeAll([_clickhouse_it_module])
+//                clickhouse_it_module = [_clickhouse_it_module]
+//            }
+        }
+
+        Collections.reverse(modules)
+        println "final modules: ${modules}"
+
+        def taskQueue = modules as LinkedBlockingQueue
+//        def chitQueue = clickhouse_it_module as LinkedBlockingQueue
+
+        def worker1 = createWorker("UTest-Stage-1", taskQueue)
+        def worker2 = createWorker("UTest-Stage-2", taskQueue)
+        def worker3 = createWorker("UTest-Stage-3", taskQueue)
+//        def worker4 = createWorker("UTest-Stage-4", chitQueue)
+
+        def allTestStages = worker1 + worker2 + worker3
+        allTestStages.failFast = true
+        parallel allTestStages
+    }
+}
+
+def createWorker(String stage, LinkedBlockingQueue taskQueue) {
+    def jvmArgs = defaultJvmArgs()
+    return [(stage): {
+        container('maven') {
+            script {
+                sh script: """
+                    if [ ! -d ${stage} ]; then
+                        mkdir ${stage} && cp -arf ./sourcecode/* ./${stage}/
+                    fi
+                """
+                def item = taskQueue.poll()
+                def _count = 0
+                while (item != null) {
+                    println "retry [${_count}], jvmArgs: ${jvmArgs}, stage: ${stage}, item: ${item}"
+                    try {
+                        sh script: """
+                            cd ./${stage}
+                            mvn clean test --fail-at-end \
+                            -pl ${item} \
+                            -DfailIfNoTests=false \
+                            -Duser.timezone=GMT+8 ${jvmArgs}
+                        """
+                        item = taskQueue.poll()
+                    } catch (ex) {
+                        echo """Exception: ${ex.toString()}\n \
+                                ooops - caught: ${ex.class}\n \
+                                ooops - with msg: ${ex.message}\n \
+                                ooops - backtrace: ${ex.stackTrace}"""
+                        jvmArgs = ''
+                        _count += 1
+                    }
+
+                    if (_count == 2) {
+                        error 'Retry twice and still fail'
+                    }
+                }
+            }
+        }
+    }]
+}
+
+def defaultJvmArgs() {
+    return params.args.isEmpty() ? '-DskipBuild=true' : params.args
+}
