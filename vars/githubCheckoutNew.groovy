03799ddb554d6d660741bcd863eec093cb78af41
lexmeng <liming.meng@kyligence.io>
2022-11-05 23:38:51 +0800

    refine the gitCheckout

--- vars/githubCheckoutNew.groovy
+++ vars/githubCheckoutNew.groovy
@@ -1,27 +1,12 @@
 def call(String project, String branch = null, String schema = 'git', String remoteHost = '10.1.2.192', refspec = '+refs/heads/*:refs/remotes/origin/*', certificate=null, repo=null) {
     timestamps {
-        if (sha1) {
-            println "checkout from pull request."
-            def pullNumber = matchPullNumber(sha1)
-            checkout([$class: 'GitSCM', 
-                      branches: [[name: sha1]], 
-                      extensions: [[$class: 'CloneOption', depth: 1, honorRefspec: true, noTags: true, reference: '', shallow: true, timeout: 3]], 
-                      userRemoteConfigs: [[credentialsId: "${certificate ?: 'kyligence-git'}",
-                                          name: 'origin', 
-                                          refspec: "+refs/pull/${pullNumber}/*:refs/remotes/origin/pr/${pullNumber}/*", 
-                                          url: "${schema ?: 'git'}://${remoteHost ?: '10.1.2.192'}/${repo ?: 'Kyligence'}/${project}.git"]]])
-        } else if (params.branch) {
-            println "checkout from specified branch."
-            checkout([$class: 'GitSCM', 
-                      branches: [[name: params.branch]], 
-                      extensions: [[$class: 'CloneOption', depth: 1, honorRefspec: true, noTags: true, reference: '', shallow: true, timeout: 3]], 
-                      userRemoteConfigs: [[credentialsId: "${certificate ?: 'kyligence-git'}", 
-                                          name: 'origin', 
-                                          refspec: "+refs/heads/${params.branch}:refs/remotes/origin/${params.branch}", 
-                                          url: "${schema ?: 'git'}://${remoteHost ?: '10.1.2.192'}/${repo ?: 'Kyligence'}/${project}.git"]]])
-        } else {
-            throw new RuntimeException("unknown branch")
-        }
+        checkout([
+            $class: 'GitSCM', 
+            branches: [[name: "${branch ?: sha1}"]], 
+            extensions: [[$class: 'CloneOption', depth: 1, honorRefspec: true, noTags: true, reference: '', shallow: true, timeout: 3]], 
+            userRemoteConfigs: [[credentialsId: "${certificate ?: 'kyligence-git'}", name: 'origin', 
+                                refspec: "${branch ? '+refs/heads/' + branch :'refs/remotes/origin/' + branch : '+refs/pull/'+matchPullNumber(sha1)+'/*:refs/remotes/origin/pr/'+matchPullNumber(sha1)+'/*'}", 
+                                url: "${schema ?: 'git'}://${remoteHost ?: '10.1.2.192'}/${repo ?: 'Kyligence'}/${project}.git"]]])
         checkLicensePatch()
     }
 }
