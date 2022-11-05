4ba4c4c2f6e5633ba6e489d0938fa7db32056cef
lexmeng <liming.meng@kyligence.io>
2022-11-06 00:46:57 +0800

    refine the gitCheckout

--- vars/githubCheckoutNew.groovy
+++ vars/githubCheckoutNew.groovy
@@ -1,11 +1,12 @@
 def call(String project, String branch = null, String schema = 'git', String remoteHost = '10.1.2.192', refspec = '+refs/heads/*:refs/remotes/origin/*', certificate=null, repo=null) {
+    def _refspec = ''
     timestamps {
         checkout([
             $class: 'GitSCM', 
             branches: [[name: "${branch ?: sha1}"]], 
             extensions: [[$class: 'CloneOption', depth: 1, honorRefspec: true, noTags: true, reference: '', shallow: true, timeout: 3]], 
             userRemoteConfigs: [[credentialsId: "${certificate ?: 'kyligence-git'}", name: 'origin', 
-                                refspec: "${branch ? '+refs/heads/' + branch :'refs/remotes/origin/' + branch : '+refs/pull/'+matchPullNumber(sha1)+'/*:refs/remotes/origin/pr/'+matchPullNumber(sha1)+'/*'}", 
+                                refspec: "${branch ? '+refs/heads/'+branch':refs/remotes/origin/'+branch : '+refs/pull/'+matchPullNumber(sha1)+'/*:refs/remotes/origin/pr/'+matchPullNumber(sha1)+'/*')}", 
                                 url: "${schema ?: 'git'}://${remoteHost ?: '10.1.2.192'}/${repo ?: 'Kyligence'}/${project}.git"]]])
         checkLicensePatch()
     }
