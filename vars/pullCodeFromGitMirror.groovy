0464ef0ba0fd2278448a2410ab586ec4305f2e13
Lex Meng <liming.meng@kyligence.io>
2022-11-07 10:58:57 +0800

    fix git mirror

--- vars/pullCodeFromGitMirror.groovy
+++ vars/pullCodeFromGitMirror.groovy
@@ -4,38 +4,27 @@
  * @param branch  the branch which will pull
  * @param fromRemote skip the mirror and pull the code from the remote
  */
-def call(String project, String branch, Boolean fromRemote=false, String repo='Kyligence', String refspec = '+refs/heads/*:refs/remotes/origin/*', String certificate=null, Boolean syncGitMirror=true) {
-    // println "pullCodeFromGitMirror -> params: project: ${project}, branch/commit: ${branch}, fromRemote: ${fromRemote)}, repo: ${repo}, syncGitMirror: ${syncGitMirror},refspec ${refspec}, certificate: ${certificate}"
+def call(String project, String branch, Boolean fromRemote=false, String repo='Kyligence', refspec = '+refs/heads/*:refs/remotes/origin/*', certificate=null) {
+    println "pullCodeFromGitMirror -> params: project: ${project}, branch/commit: ${branch}, fromRemote: ${fromRemote}, repo: ${repo}, refspec ${refspec}, certificate: ${certificate}"
     if (!branch) {
         error 'branch 参数不能为空'
     }
 
     if(!fromRemote) { // 从本地镜像获取
-        // sshCmdWithCredentials(
-        //     "sh /var/lib/git/pull-repo.sh ${project}",
-        //     'private_git',
-        //     ['name':'localhost', 'host':'10.1.2.192', 'allowAnyHosts': true]
-        // )
-        if(syncGitMirror) {
-            syncGitMirror(project)
-        }
+        sshCmdWithCredentials(
+            "sh /var/lib/git/pull-repo.sh ${project}",
+            'private_git',
+            ['name':'localhost', 'host':'10.1.2.192', 'allowAnyHosts': true]
+        )
 
         println "checkout ${project} from git mirror at local, it'll be retry 3 times"
         retry(3) {
-            githubCheckoutNew project, branch, 'git', '10.1.2.192', refspec, certificate, repo
+            githubCheckout project, branch, 'git', '10.1.2.192', refspec, certificate, repo
         }
     } else {
         println "checkout ${project} from github from remote, it'll be retry 3 times"
         retry(3) {
-            githubCheckoutNew project, branch, 'https', 'github.com', refspec, certificate, repo
+            githubCheckout project, branch, 'https', 'github.com', refspec, certificate, repo
         }
     }
 }
-
-// def syncGitMirror(String project) {
-//     sshCmdWithCredentials(
-//         "sh /var/lib/git/pull-repo.sh ${project}",
-//         'private_git',
-//         ['name':'localhost', 'host':'10.1.2.192', 'allowAnyHosts': true]
-//     )
-// }
