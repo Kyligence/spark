5179a512b5a9858ab87578e434a50c1be425bc9b
lexmeng <liming.meng@kyligence.io>
2022-11-25 16:59:19 +0800

    fix pull code

--- vars/pullCodeFromGitMirrorNew.groovy
+++ vars/pullCodeFromGitMirrorNew.groovy
@@ -5,7 +5,7 @@
  * @param fromRemote skip the mirror and pull the code from the remote
  */
 def call(String project, String branch, Boolean fromRemote=false, String repo='Kyligence', Boolean syncGitMirror=true, String certificate=null) {
-    println "pullCodeFromGitMirror -> params: project: ${project}, branch/commit: ${branch}, fromRemote: ${fromRemote}, repo: ${repo}, syncGitMirror: ${syncGitMirror}, certificate: ${certificate}"
+    println "pullCodeFromGitMirrorNew -> params: project: ${project}, branch/commit: ${branch}, fromRemote: ${fromRemote}, repo: ${repo}, syncGitMirror: ${syncGitMirror}, certificate: ${certificate}"
     if (!branch) {
         error 'branch 参数不能为空'
     }
