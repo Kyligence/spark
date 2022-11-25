c5e92af90135639092f0705971565ed2e5712bdd
lexmeng <liming.meng@kyligence.io>
2022-11-25 16:38:48 +0800

    fix pull code

--- vars/pullCodeFromGitMirrorNew.groovy
+++ vars/pullCodeFromGitMirrorNew.groovy
@@ -17,12 +17,14 @@ def call(String project, String branch, Boolean fromRemote=false, String repo='K
 
         println "checkout ${project} from git mirror at local, it'll be retry 3 times"
         retry(3) {
-            githubCheckoutNew project, branch, 'git', '10.1.2.192', null, certificate, repo
+            // githubCheckoutNew project, branch, 'git', '10.1.2.192', null, certificate, repo
+            githubCheckout project, branch, 'git', '10.1.2.192', refspec, certificate, repo
         }
     } else {
         println "checkout ${project} from github from remote, it'll be retry 3 times"
         retry(3) {
-            githubCheckoutNew project, branch, 'https', 'github.com', null, certificate, repo
+            // githubCheckoutNew project, branch, 'https', 'github.com', null, certificate, repo
+            githubCheckout project, branch, 'https', 'github.com', refspec, certificate, repo
         }
     }
 }
