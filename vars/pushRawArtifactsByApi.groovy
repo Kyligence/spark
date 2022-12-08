ead9f114cbb57fc337c7335590e99a5d14af1c2e
lexmeng <liming.meng@kyligence.io>
2022-12-09 01:19:50 +0800

    set curl -k for no check certificate

--- vars/pushRawArtifactsByApi.groovy
+++ vars/pushRawArtifactsByApi.groovy
@@ -1,11 +1,11 @@
 def call(repository,directory,file,filename,user,passwd){
- sh """
+ sh '''
    curl -k -X POST "https://repo-ofs.kyligence.com/service/rest/v1/components?repository=${repository}" \
       -H "accept: application/json" \
       -H "Content-Type: multipart/form-data" \
       -F "raw.directory=${directory}" \
       -F "raw.asset1=@${file};type=application/java-archive" \
       -F "raw.asset1.filename=${filename}" \
-      -u "${user}":"${passwd}"
-"""
+      -u $user:$passwd
+'''
 }
