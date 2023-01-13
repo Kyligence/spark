f808285248be58e1129e299cfc75b225bb697bca
Cheng Zuo <Cheng.zuo@kyligence.io>
2023-01-13 11:52:24 +0800

    fix bug (#346)
    Co-authored-by: zuoc <cheng.zuo@kyligence>

--- vars/testNewtenDynamicParallelTestInGcp.groovy
+++ vars/testNewtenDynamicParallelTestInGcp.groovy
@@ -81,7 +81,7 @@ Map<String, List<String>> evalTestModules() {
                 """, returnStdout: true)
                         .trim()
                         .split("\n")
-                        .findAll({ !it.startsWith("Downloaded from") && !it.startsWith("Downloading from") })
+                        .findAll({ !it.startsWith("Downloaded from") && !it.startsWith("Downloading from") && !it.startsWith("Downloadingfrom") && !it.startsWith("Progress") })
                         .collect({ parentModule.isEmpty() ? it.trim() : "${parentModule}/${it.trim()}" })
 
                 println "child modules: ${childModules}"
