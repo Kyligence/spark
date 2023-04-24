deeb4cc811c568e9cd6b530cb798ba742eb0a0c3
lexmeng <liming.meng@kyligence.io>
2023-04-24 17:39:35 +0800

    add npm cache clean

--- vars/buildNewten.groovy
+++ vars/buildNewten.groovy
@@ -23,7 +23,10 @@ def call(String version, boolean noSpark = false, String docs_commitid = 'latest
         wget https://repo-ofs.kyligence.com/repository/raw-tars-hosted/io.kyligence.ke/postgresql10-libs-10.8-1PGDG.rhel8.x86_64.rpm --directory-prefix=build/postgresql/ --no-check-certificate
         wget https://repo-ofs.kyligence.com/repository/raw-tars-hosted/io.kyligence.ke/postgresql10-server-10.8-1PGDG.rhel8.x86_64.rpm --directory-prefix=build/postgresql/ --no-check-certificate
     """
-    sh "npm cache verify"
+    sh """
+        npm cache clean --force
+        npm cache verify
+    """
 
     retry (2) {
         if (noSpark) {
