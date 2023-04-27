32e3d3d522df9308b52944050ad2c0bcf4f3e1b1
zihan.deng <yumening7@users.noreply.github.com>
2023-04-27 12:09:45 +0800

    Devops-1825-new (#441)
    * add check author
    
    * fix script
    
    * update inport
    
    * add println
    
    * update jenkinsfile
    
    * update jenkins
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * delete log
    
    * no message
    
    * no message
    
    * update jenkins file
    
    * fix step run script
    
    * add print cluster
    
    * update step and 4xuser
    
    ---------
    
    Co-authored-by: zihan.deng <zihan.deng@kyligence.io>

--- vars/ke4xHelper.groovy
+++ vars/ke4xHelper.groovy
@@ -38,7 +38,73 @@ def deployToServer(hostIp, remote_name, package_name,
                 echo "【${kepackage_dir}】文件夹不存在"
                 mkdir -p ${kepackage_dir}
             fi
-            
+
+            if [ -f "${kepackage_dir}/${package_name}" ]
+            then
+                echo "【${kepackage_dir}/${package_name}】本机存在"
+                cp ${kepackage_dir}/${package_name} .
+            else
+                echo "【${kepackage_dir}/${package_name}】本机不存在，尝试从10.0.0.28获取"
+                scp 4xuser@10.0.0.28:${kepackage_dir}/${package_name}  .
+            fi
+
+            cd ${remote_dir}
+            rm -rf new_kedir
+            mkdir new_kedir
+            tar -zxvf dist/${package_name} --strip-components 1 -C new_kedir
+            if test -d ${ke_workdir}
+            then
+                bash ${ke_workdir}/bin/kylin.sh stop
+            else
+                echo "The KE server is not running"
+            fi
+            bash new_kedir/bin/upgrade.sh ${ke_workdir} --silent
+            touch ${ke_workdir}/bin/check-env-bypass
+            echo "skip check env"
+            bash ${ke_workdir}/bin/kylin.sh start
+            cat ${ke_workdir}/commit_SHA1
+            echo "upgrade ke-env finished"
+            """
+    }
+}
+
+def deployToServerStep(hostIp, remote_name, package_name,
+                   remote_dir = '/home/step/data/4.5test', kepackage_dir = '/home/step/data/devops', ke_workdir = 'Kyligence-Enterprise-newten-current') {
+    println """deployToServer -> hostIp[${hostIp}],remote_name[${remote_name}],package_name[${package_name}],
+        remote_dir[${remote_dir}],kepackage_dir[${kepackage_dir}],ke_workdir[${ke_workdir}]"""
+    script {
+        def remote = [:]
+        remote.name = remote_name
+        withCredentials([usernamePassword(credentialsId: 'azure-step', usernameVariable: 'REMOTE_USERNAME', passwordVariable: 'REMOTE_PASSWORD')]) {
+            remote.user = REMOTE_USERNAME
+            remote.password = REMOTE_PASSWORD
+        }
+        remote.host = hostIp
+        remote.allowAnyHosts = true
+        sshCommand remote: remote, command: """
+            #! /bin/bash
+            set -e
+            if test -d ${remote_dir}
+            then
+                ls -alth ${remote_dir}
+            else
+                mkdir -p ${remote_dir}
+            fi
+
+            cd ${remote_dir}
+            rm -rf dist
+            mkdir dist
+            cd dist
+
+            if test -d ${kepackage_dir}
+            then
+                echo "【${kepackage_dir}】文件夹存在"
+                ls -alth ${kepackage_dir}
+            else
+                echo "【${kepackage_dir}】文件夹不存在"
+                mkdir -p ${kepackage_dir}
+            fi
+
             if [ -f "${kepackage_dir}/${package_name}" ]
             then
                 echo "【${kepackage_dir}/${package_name}】本机存在"
@@ -46,7 +112,7 @@ def deployToServer(hostIp, remote_name, package_name,
             else
                 echo "【${kepackage_dir}/${package_name}】本机不存在，尝试从10.0.0.28获取"
                 whoami
-                scp 4xuser@10.0.0.28:${kepackage_dir}/${package_name}  .
+                scp step@10.0.0.28:${kepackage_dir}/${package_name}  .
                 echo "【复制完成】"
             fi
             cd ${remote_dir}
@@ -68,8 +134,15 @@ def deployToServer(hostIp, remote_name, package_name,
                 echo "【bash】"
             else
                 echo "The KE server is not running"
+                echo ${package_name}
+                echo `ls`
+                tar -zxvf dist/${package_name} -C ${remote_dir}
+                echo \$(echo "Kyligence-Enterprise-\\$package_name" | awk -F "-" '{print \$1"-"\$2"-"\$4"-"\$5}')
+                mv \$(echo "Kyligence-Enterprise-\\$package_name" | awk -F "-" '{print \$1"-"\$2"-"\$4"-"\$5}') ${ke_workdir}
             fi
-            # bash -x new_kedir/bin/upgrade.sh ${ke_workdir} --silent
+            set +e
+            bash -x new_kedir/bin/upgrade.sh ${ke_workdir} --silent
+            set -e
             echo "【bash2】"
             touch ${ke_workdir}/bin/check-env-bypass
             echo "【touch】"
@@ -82,16 +155,151 @@ def deployToServer(hostIp, remote_name, package_name,
             wget https://repo-ofs.kyligence.com/repository/raw-tar-qa-hosted/libs/mysql-connector-j-8.0.32.jar --directory-prefix=${ke_workdir}/lib/ext/
             wget https://repo-ofs.kyligence.com/repository/raw-tar-qa-hosted/libs/mysql-connector-j-8.0.32.jar --directory-prefix=${ke_workdir}/spark/jars/
             wget https://repo-ofs.kyligence.com/repository/raw-tar-qa-hosted/license/KE4/LICENSE --directory-prefix=${ke_workdir}/
-            bash -x ${ke_workdir}/bin/kylin.sh start
-            echo "【bash3】"
+
+            echo "kylin.metadata.url=step@jdbc,driverClassName=com.mysql.jdbc.Driver,url=jdbc:mysql://10.0.0.43:3306/kylin,username=kylin,password=KyLin@QA_2022
+            kylin.env.hdfs-working-dir=/user/step/kylin
+            kylin.env.zookeeper-connect-string=10.0.0.7:2181,10.0.0.8:2181,10.0.0.9:2181
+
+
+            kylin.storage.columnar.spark-conf.spark.yarn.queue=root.step
+            kylin.engine.spark-conf.spark.yarn.queue=root.step
+            kylin.streaming.spark-conf.spark.yarn.queue=root.step
+
+            kylin.storage.columnar.spark-conf.spark.yarn.am.memory=4g
+            kylin.storage.columnar.spark-conf.spark.executor.cores=2
+            kylin.storage.columnar.spark-conf.spark.executor.instances=1
+
+            spring.session.store-type=jdbc
+            kylin.server.mode=query
+            server.port=7072
+
+            #优化策略，定时垃圾清理
+            kylin.index.optimization-level=3
+            kylin.metadata.ops-cron=0 0/10 * * * ?
+
+            #元数据明文展示
+            kylin.metadata.compress.enabled=false
+            #不随机密码
+            kylin.metadata.random-admin-password.enabled=false
+
+
+            kylin.streaming.enabled=true
+            #streaming
+            kylin.streaming.spark-conf.spark.shuffle.io.maxRetries=10
+            kylin.streaming.spark-conf.spark.network.timeout=600
+            kylin.streaming.spark-conf.spark.driver.memoryOverhead=2048
+            kylin.streaming.spark-conf.spark.executor.memoryOverhead=2048
+            kylin.streaming.spark-conf.spark.shuffle.io.retryWait=60
+            kylin.streaming.spark-conf.spark.executor.heartbeatInterval=300
+            kylin.streaming.spark-conf.spark.serializer=org.apache.spark.serializer.KryoSerializer
+            kylin.streaming.spark-conf.spark.speculation=true
+            kylin.streaming.spark-conf.spark.locality.wait=2s
+            kylin.streaming.spark-conf.spark.speculation=true
+            kylin.streaming.spark-conf.spark.speculation.interval=900s
+            kylin.streaming.spark-conf.spark.speculation.quantile=0.9
+            kylin.streaming.spark-conf.spark.speculation.multiplier=1.5
+            kylin.streaming.job.retry.interval=5m
+            #kylin.streaming.shutdown-timeout=5m
+            #kylin.streaming.startup-timeout=5m
+            kylin.streaming.kafka.max-rate-per-partition=200
+            kylin.streaming.spark-conf.spark.sql.hive.metastore.version=1.2.2
+            kylin.streaming.spark-conf.spark.sql.hive.metastore.jars=${ke_workdir}/spark/hive_1_2_2/*
+            #kylin.env.zookeeper-max-retries=6
+            #kylin.metadata.compress.enabled=false
+            #kylin.index.optimization-level=3
+            #kylin.query.convert-sum-expression-enabled=false
+            #kylin.engine.spark-conf.spark.executor.instances=3" > /tmp/kylin.properties.override.tmp
+
+            cat /tmp/kylin.properties.override.tmp > ${ke_workdir}/conf/kylin.properties.override
+
+            export PATH=$PATH:/usr/bin:/usr/sbin
+            if [ ${hostIp} == "159.27.224.211" ]; then
+              rm -rf 7000
+              rm -rf 7001
+              rm -rf 7002
+              cp -r ${ke_workdir} 7000
+              cp -r ${ke_workdir} 7001
+              cp -r ${ke_workdir} 7002
+              sed -i 's/sql.hive.metastore.jars=Kyligence-Enterprise-newten-current/sql.hive.metastore.jars=7000/g' 7000/conf/kylin.properties.override
+              sed -i 's/sql.hive.metastore.jars=Kyligence-Enterprise-newten-current/sql.hive.metastore.jars=7001/g' 7001/conf/kylin.properties.override
+              sed -i 's/sql.hive.metastore.jars=Kyligence-Enterprise-newten-current/sql.hive.metastore.jars=7002/g' 7002/conf/kylin.properties.override
+              sed -i 's/server.port=7072/server.port=7000/g' 7000/conf/kylin.properties.override
+              sed -i 's/server.port=7072/server.port=7001/g' 7001/conf/kylin.properties.override
+              sed -i 's/server.port=7072/server.port=7002/g' 7002/conf/kylin.properties.override
+              fuser -k 7000/tcp
+              fuser -k 7001/tcp
+              fuser -k 7002/tcp
+              bash -x 7000/bin/kylin.sh start
+              bash -x 7001/bin/kylin.sh start
+              bash -x 7002/bin/kylin.sh start
+            elif [ ${hostIp} == "159.27.224.168" ]; then
+              rm -rf 7003
+              rm -rf 7004
+              rm -rf 7005
+              cp -r ${ke_workdir} 7003
+              cp -r ${ke_workdir} 7004
+              cp -r ${ke_workdir} 7005
+              sed -i 's/sql.hive.metastore.jars=Kyligence-Enterprise-newten-current/sql.hive.metastore.jars=7003/g' 7003/conf/kylin.properties.override
+              sed -i 's/sql.hive.metastore.jars=Kyligence-Enterprise-newten-current/sql.hive.metastore.jars=7004/g' 7004/conf/kylin.properties.override
+              sed -i 's/sql.hive.metastore.jars=Kyligence-Enterprise-newten-current/sql.hive.metastore.jars=7005/g' 7005/conf/kylin.properties.override
+              sed -i 's/server.port=7072/server.port=7003/g' 7003/conf/kylin.properties.override
+              sed -i 's/server.port=7072/server.port=7004/g' 7004/conf/kylin.properties.override
+              sed -i 's/server.port=7072/server.port=7005/g' 7005/conf/kylin.properties.override
+              fuser -k 7003/tcp
+              fuser -k 7004/tcp
+              fuser -k 7005/tcp
+              bash -x 7003/bin/kylin.sh start
+              bash -x 7004/bin/kylin.sh start
+              bash -x 7005/bin/kylin.sh start
+            elif [ ${hostIp} == "159.27.120.134" ]; then
+              echo "【start rm】"
+              rm -rf 7006
+              rm -rf 7007
+              rm -rf 7008
+              echo "【start cp】"
+              cp -r ${ke_workdir} 7006
+              cp -r ${ke_workdir} 7007
+              cp -r ${ke_workdir} 7008
+              echo "【start sed】"
+              sed -i 's/sql.hive.metastore.jars=Kyligence-Enterprise-newten-current/sql.hive.metastore.jars=7006/g' 7006/conf/kylin.properties.override
+              sed -i 's/sql.hive.metastore.jars=Kyligence-Enterprise-newten-current/sql.hive.metastore.jars=7007/g' 7007/conf/kylin.properties.override
+              sed -i 's/sql.hive.metastore.jars=Kyligence-Enterprise-newten-current/sql.hive.metastore.jars=7008/g' 7008/conf/kylin.properties.override
+              sed -i 's/server.port=7072/server.port=7006/g' 7006/conf/kylin.properties.override
+              sed -i 's/server.port=7072/server.port=7007/g' 7007/conf/kylin.properties.override
+              sed -i 's/server.port=7072/server.port=7008/g' 7008/conf/kylin.properties.override
+              set +e
+              echo "【start fuser 7006】"
+              fuser -k 7006/tcp
+              echo "【start fuser 7007】"
+              fuser -k 7007/tcp
+              echo "【start fuser 7008】"
+              fuser -k 7008/tcp
+              set -e
+              echo "【start ke】"
+              bash -x 7006/bin/kylin.sh start
+              bash -x 7007/bin/kylin.sh start
+              bash -x 7008/bin/kylin.sh start
+              echo "【end ke】"
+            else
+              echo "hostIp error : hostIp = ${hostIp}"
+              exit 1
+            fi
+            echo '-----'
+            echo pwd
+            echo '-----'
             cat ${ke_workdir}/commit_SHA1
+            echo '-----'
             echo "upgrade ke-env finished"
+            echo '-----'
             """
+//             bash -x ${ke_workdir}/bin/kylin.sh start
+//             new_ke_workdir=\$(echo "Kyligence-Enterprise-${package_name#*-}" | sed 's/\(^[^-]*-[^-]*-[^-]*-[^-]*\).*$/\1/')
+// new_ke_workdir=\$(echo "Kyligence-Enterprise-\\$package_name" | awk -F "-" '{print \$1"-"\$2"-"\$4"-"\$5}')
     }
 }
 
 def deployToServerWithCH(hostIp, remote_name, package_name,
-                         remote_dir = '/home/step/data/CH/CH_STEP', kepackage_dir = '/home/4xuser/data/devops', ke_workdir = 'Kyligence-Enterprise-4.5-CKstep-do-not-delete') {
+                         remote_dir = '/home/step/data/CH/CH_STEP', kepackage_dir = '/home/step/data/devops', ke_workdir = 'Kyligence-Enterprise-4.5-CKstep-do-not-delete') {
     println """deployToServerWithCH -> hostIp[${hostIp}],package_name[${package_name}],
         kepackage_dir[${kepackage_dir}],remote_dir[${remote_dir}],ke_workdir[${ke_workdir}]"""
     script {
@@ -116,7 +324,7 @@ def deployToServerWithCH(hostIp, remote_name, package_name,
             fi
             cd ${remote_dir}
             rm -f ke_package/K*.tar.gz
-            scp 4xuser@10.0.0.28:${kepackage_dir}/${package_name}  ke_package/
+            scp step@10.0.0.28:${kepackage_dir}/${package_name}  ke_package/
             if test -d new_kedir
             then
                 rm -rf new_kedir
@@ -157,14 +365,24 @@ def stepRun(planName, envStage, productLine, versionName, check_time = 600, time
                 rm -rf *
                 ls -lh ${env.WORKSPACE}/qa/4X_STEP_TEST/
                 cp -r ${env.WORKSPACE}/qa/4X_STEP_TEST/* /opt
+                echo "envStage = ${envStage}"
+                echo "check_time = ${check_time}"
+                echo "【${productLine} ${versionName} ${planName} ${envStage} ${check_time} ${REMOTE_USERNAME} ${REMOTE_PASSWORD}】"
+                cd /opt/qa_auto
+                sed -i '1s/^/\\n/' excute_test_plan.py
+                sed -i "/except IndexError:/a\\        print(traceback.format_exc())" excute_test_plan.py
+                sed -i 's/import urllib3/import urllib3\\nimport traceback/g' excute_test_plan.py
+                sed -i 's/+ cluster +/+ f"{cluster}" +/g' excute_test_plan.py
+                sed -i 's/, cluster)/, f"{cluster}")/g' excute_test_plan.py
+                cat excute_test_plan.py
+                python3 -x excute_test_plan.py 'first' ${planName} ${envStage} ${productLine} ${versionName} ${check_time} ${REMOTE_USERNAME} ${REMOTE_PASSWORD} '10.1.3.18'
             """
-            retry(3) {
-                sh script: """
-                    set -e
-                    cd /opt/qa_auto
-                    python3 excute_test_plan.py 'first' ${productLine} ${versionName} ${planName} ${envStage} ${check_time} ${REMOTE_USERNAME} ${REMOTE_PASSWORD} '10.1.3.18'
-                """
-            }
+//             retry(3) {
+//                 sh script: """
+//                     cd /opt/qa_auto
+//                     python3 -x excute_test_plan.py 'first' ${planName} ${envStage} ${productLine} ${versionName} ${check_time} ${REMOTE_USERNAME} ${REMOTE_PASSWORD} '10.1.3.18'
+//                 """
+//             }
         }
     }
 }
@@ -181,7 +399,7 @@ def notifyToTester(title, ip, file_name,  emails,failure = false) {
         case 'upload':
             _title = 'Upload'
             _content = _content + """
-                <p>It saved in the path of [/home/4xuser/data/devops], please check it and use! </p>
+                <p>It saved in the path of [/home/step/data/devops], please check it and use! </p>
             """
             break
         case 'deploy':
