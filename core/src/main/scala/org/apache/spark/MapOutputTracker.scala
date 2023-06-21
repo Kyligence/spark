cdb003d6ab22e0fb4a59cb2b35b4c67115626583
Kun Wan <wankun@apache.org>
2023-06-21 22:31:19 +0800

    [SPARK-44109][CORE] Remove duplicate preferred locations of each RDD partition
    ### What changes were proposed in this pull request?
    
    DAGScheduler will get the preferred locations for each RDD partition and try to allocate the task on the preferred locations.
    
    We can remove the duplicate preferred locations to save memory.
    
    For example. reduce 0 needs to fetch map0 output and map1 output in host-A, then the preferred locations can be Array("host-A").
    
    ### Why are the changes needed?
    
    To save driver memory for large job, too many preferred locations may cause OOM.
    
    ```
    2023-06-19T14:14:18.501-0700: [Full GC (Allocation Failure) 2023-06-19T14:14:18.501-0700: [Class Histogram (before full gc):
     num     #instances         #bytes  class name
    ----------------------------------------------
       1:    2980686820    95381978240  scala.collection.immutable.$colon$colon
       2:    2978345818    71480299632  org.apache.spark.scheduler.HostTaskLocation
       3:      24388384     3963604560  [C
       4:      24265128      776484096  java.lang.String
       5:        368944      560810384  [Ljava.lang.Object;
       6:       2927846      421609824  java.net.URI
       7:      16239585      389750040  java.lang.Integer
       8:       2540273      243866208  org.apache.hadoop.fs.FileStatus
       9:        676141      203208736  [B
      10:       3845164      184567872  java.util.Hashtable$Entry
      11:       3174033      152353584  java.util.concurrent.ConcurrentHashMap$Node
    ```
    
    ### Does this PR introduce _any_ user-facing change?
    
    No
    
    ### How was this patch tested?
    
    Added UT
    
    Closes #41676 from wankunde/preferred_locations.
    
    Lead-authored-by: Kun Wan <wankun@apache.org>
    Co-authored-by: wankun <wankunde@163.com>
    Signed-off-by: Dongjoon Hyun <dongjoon@apache.org>
    
    (cherry picked from commit 6a70f756dd110f5b5a1324c042585fd6b992cc21)

--- core/src/main/scala/org/apache/spark/MapOutputTracker.scala
+++ core/src/main/scala/org/apache/spark/MapOutputTracker.scala
@@ -1027,7 +1027,7 @@ private[spark] class MapOutputTrackerMaster(
           val blockManagerIds = getLocationsWithLargestOutputs(dep.shuffleId, partitionId,
             dep.partitioner.numPartitions, REDUCER_PREF_LOCS_FRACTION)
           if (blockManagerIds.nonEmpty) {
-            blockManagerIds.get.map(_.host)
+            blockManagerIds.get.map(_.host).distinct.toSeq
           } else {
             Nil
           }
@@ -1113,7 +1113,7 @@ private[spark] class MapOutputTrackerMaster(
         if (startMapIndex < endMapIndex &&
           (startMapIndex >= 0 && endMapIndex <= statuses.length)) {
           val statusesPicked = statuses.slice(startMapIndex, endMapIndex).filter(_ != null)
-          statusesPicked.map(_.location.host).toSeq
+          statusesPicked.map(_.location.host).distinct.toSeq
         } else {
           Nil
         }
