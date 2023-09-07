ac5f9ee03a62be71c6b7e76cb747e275d436650f
jiawei.li <1019037687@qq.com>
2023-09-07 16:35:42 +0800

    KE-42576 fix ivy ut

--- core/src/test/scala/org/apache/spark/deploy/SparkSubmitSuite.scala
+++ core/src/test/scala/org/apache/spark/deploy/SparkSubmitSuite.scala
@@ -671,6 +671,8 @@ class SparkSubmitSuite
 
   // SPARK-7287
   test("includes jars passed in through --packages") {
+    System.setProperty("javax.xml.parsers.SAXParserFactory",
+      "com.sun.org.apache.xerces.internal.jaxp.SAXParserFactoryImpl")
     val unusedJar = TestUtils.createJarWithClasses(Seq.empty)
     val main = MavenCoordinate("my.great.lib", "mylib", "0.1")
     val dep = MavenCoordinate("my.great.dep", "mylib", "0.1")
@@ -691,6 +693,8 @@ class SparkSubmitSuite
   }
 
   test("includes jars passed through spark.jars.packages and spark.jars.repositories") {
+    System.setProperty("javax.xml.parsers.SAXParserFactory",
+      "com.sun.org.apache.xerces.internal.jaxp.SAXParserFactoryImpl")
     val unusedJar = TestUtils.createJarWithClasses(Seq.empty)
     val main = MavenCoordinate("my.great.lib", "mylib", "0.1")
     val dep = MavenCoordinate("my.great.dep", "mylib", "0.1")
