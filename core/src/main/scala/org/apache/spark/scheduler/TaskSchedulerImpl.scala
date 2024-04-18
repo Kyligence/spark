bbc5a6503ffcbd1386d1fcbf093467bfc436c66f
huangsheng <huangshengtx@163.com>
2024-04-18 14:09:02 +0800

    opt code

--- core/src/main/scala/org/apache/spark/scheduler/TaskSchedulerImpl.scala
+++ core/src/main/scala/org/apache/spark/scheduler/TaskSchedulerImpl.scala
@@ -872,7 +872,7 @@ private[spark] class TaskSchedulerImpl(
   def handleSuccessfulTask(
       taskSetManager: TaskSetManager,
       tid: Long,
-      taskResult: TaskResult[_]): Unit = synchronized {
+      taskResult: DirectTaskResult[_]): Unit = synchronized {
     taskSetManager.handleSuccessfulTask(tid, taskResult)
   }
 
