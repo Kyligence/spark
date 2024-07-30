c1bde7f0017442632754ef48b2d4cdecd17b6ee5
Jiaheng Tang <jiaheng.tang@databricks.com>
2024-07-30 11:51:46 -0400

    [SPARK-48762][SQL] Introduce clusterBy DataFrameWriter API for Python
    Introduce clusterBy DataFrameWriter API for Python.
    
    Also fix the issue that `listColumns` doesn't support `V1Table`.
    
    Introduce more ways for users to interact with clustered tables.
    
    Yes, it introduces a new PySpark DataFrame API to specify clustering columns during write operations.
    
    New unit tests.
    
    No.
    
    Closes #47452 from zedtang/clusterby-python-api.
    
    Authored-by: Jiaheng Tang <jiaheng.tang@databricks.com>
    Signed-off-by: Hyukjin Kwon <gurwls223@apache.org>

--- python/pyspark/sql/connect/plan.py
+++ python/pyspark/sql/connect/plan.py
@@ -1518,6 +1518,7 @@ class WriteOperation(LogicalPlan):
         self.mode: Optional[str] = None
         self.sort_cols: List[str] = []
         self.partitioning_cols: List[str] = []
+        self.clustering_cols: List[str] = []
         self.options: Dict[str, Optional[str]] = {}
         self.num_buckets: int = -1
         self.bucket_cols: List[str] = []
@@ -1531,6 +1532,7 @@ class WriteOperation(LogicalPlan):
             plan.write_operation.source = self.source
         plan.write_operation.sort_column_names.extend(self.sort_cols)
         plan.write_operation.partitioning_columns.extend(self.partitioning_cols)
+        plan.write_operation.clustering_columns.extend(self.clustering_cols)
 
         if self.num_buckets > 0:
             plan.write_operation.bucket_by.bucket_column_names.extend(self.bucket_cols)
@@ -1586,6 +1588,7 @@ class WriteOperation(LogicalPlan):
             f"mode='{self.mode}' "
             f"sort_cols='{self.sort_cols}' "
             f"partitioning_cols='{self.partitioning_cols}' "
+            f"clustering_cols='{self.clustering_cols}' "
             f"num_buckets='{self.num_buckets}' "
             f"bucket_cols='{self.bucket_cols}' "
             f"options='{self.options}'>"
@@ -1600,6 +1603,7 @@ class WriteOperation(LogicalPlan):
             f"mode: '{self.mode}' <br />"
             f"sort_cols: '{self.sort_cols}' <br />"
             f"partitioning_cols: '{self.partitioning_cols}' <br />"
+            f"clustering_cols: '{self.clustering_cols}' <br />"
             f"num_buckets: '{self.num_buckets}' <br />"
             f"bucket_cols: '{self.bucket_cols}' <br />"
             f"options: '{self.options}'<br />"
@@ -1612,7 +1616,8 @@ class WriteOperationV2(LogicalPlan):
         super(WriteOperationV2, self).__init__(child)
         self.table_name: Optional[str] = table_name
         self.provider: Optional[str] = None
-        self.partitioning_columns: List["ColumnOrName"] = []
+        self.partitioning_columns: List[Column] = []
+        self.clustering_columns: List[str] = []
         self.options: dict[str, Optional[str]] = {}
         self.table_properties: dict[str, Optional[str]] = {}
         self.mode: Optional[str] = None
@@ -1636,6 +1641,7 @@ class WriteOperationV2(LogicalPlan):
         plan.write_operation_v2.partitioning_columns.extend(
             [self.col_to_expr(x, session) for x in self.partitioning_columns]
         )
+        plan.write_operation_v2.clustering_columns.extend(self.clustering_columns)
 
         for k in self.options:
             if self.options[k] is None:
