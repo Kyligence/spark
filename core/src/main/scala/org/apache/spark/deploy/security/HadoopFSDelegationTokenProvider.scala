8c49e054e7f6601fb40ae8fad5e1ca2fb278bcf3
lwz9103 <lwz9103@163.com>
2024-11-18 14:23:43 +0800

    KE-44463 Fix checkstyle of wrong import order

--- core/src/main/scala/org/apache/spark/deploy/security/HadoopFSDelegationTokenProvider.scala
+++ core/src/main/scala/org/apache/spark/deploy/security/HadoopFSDelegationTokenProvider.scala
@@ -17,19 +17,20 @@
 
 package org.apache.spark.deploy.security
 
-import org.apache.hadoop.conf.Configuration
-import org.apache.hadoop.fs.{FileSystem, Path}
-import org.apache.hadoop.mapred.Master
-import org.apache.hadoop.security.token.delegation.AbstractDelegationTokenIdentifier
-import org.apache.hadoop.security.{Credentials, UserGroupInformation}
-import org.apache.spark.internal.Logging
-import org.apache.spark.internal.config._
-import org.apache.spark.security.HadoopDelegationTokenProvider
-import org.apache.spark.{SparkConf, SparkException}
-
 import scala.collection.JavaConverters._
 import scala.util.Try
 import scala.util.control.NonFatal
+
+import org.apache.hadoop.conf.Configuration
+import org.apache.hadoop.fs.{FileSystem, Path}
+import org.apache.hadoop.mapred.Master
+import org.apache.hadoop.security.{Credentials, UserGroupInformation}
+import org.apache.hadoop.security.token.delegation.AbstractDelegationTokenIdentifier
+
+import org.apache.spark.{SparkConf, SparkException}
+import org.apache.spark.internal.Logging
+import org.apache.spark.internal.config._
+import org.apache.spark.security.HadoopDelegationTokenProvider
 
 private[deploy] class HadoopFSDelegationTokenProvider
     extends HadoopDelegationTokenProvider with Logging {
